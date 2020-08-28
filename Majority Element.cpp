407f7ee9acf35414e31af203ce37ab20890848de
Mutyam Reddy Bejjanki <63692509+MutyamReddy@users.noreply.github.com>
2020-08-28 19:10:40 +0530

    Add files via upload

---
+++ Majority Element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int ele=0;
+        for(int num:nums){
+            if(count==0){
+                ele=num;
+            }
+            if(ele==num){
+                count+=1;
+            }else{
+                count-=1;
+            }
+        }
+        return ele;
+    }
+};
